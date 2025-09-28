48534b5499b06de8f7276cfbfcfd3a1e08f6194e
Yazan Kaakeh <yazanka187@gmail.com>
2025-09-28 21:54:12 +0300

    Update api.php

--- Modules/Doctor/routes/api.php
+++ Modules/Doctor/routes/api.php
@@ -1,8 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Modules\Doctor\Http\Controllers\DoctorController;
+use Modules\Doctor\Http\Controllers\DashboardController;
 
 Route::middleware(['auth:sanctum'])->prefix('v1')->group(function () {
-    Route::apiResource('doctors', DoctorController::class)->names('doctor');
+    Route::apiResource('doctors', DashboardController::class)->names('doctor');
 });
