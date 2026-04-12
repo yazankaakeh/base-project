11b611f1e8cb3d9c55b2a31978ae6ec94fc6216a
Yazan Kaakeh <yazanka187@gmail.com>
2026-04-12 20:20:06 +0300

    Update CountriesSeeder.php

--- Modules/Core/database/seeders/CountriesSeeder.php
+++ Modules/Core/database/seeders/CountriesSeeder.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Modules\Core\database\Seeders;
+namespace Modules\\Core\\Database\\Seeders;
 
 use Illuminate\Contracts\Filesystem\FileNotFoundException;
 use Illuminate\Database\Seeder;
